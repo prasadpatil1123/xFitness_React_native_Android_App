83c45da71a1c4c0dcc2ea8b5c3f2e495a8b3e2c3
prasadpatil1123 <122286888+prasadpatil1123@users.noreply.github.com>
2023-09-30 16:00:03 +0530

    Gojo2

--- src/DashBoard/GenderFile/Gender.js
+++ src/DashBoard/GenderFile/Gender.js
@@ -1,21 +1,26 @@
-import { StyleSheet, Text, View,Button } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
+import React, { useState } from 'react'
 import Male from './Male'
 import Female from './Female'
 
-const Gender = ({navigation}) => {
+const Gender = ({ navigation }) => {
+  const [selectedGender, setSelectedGender] = useState('');
+  const handleNext = () => {
+    navigation.navigate('Exercise Screen', { gender: selectedGender })
+  }
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center',backgroundColor: 'black', }}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'black', }}>
 
-      <View style={{ height: 300, width: 300, backgroundColor: '#cfcfcf', justifyContent: 'center', alignItems: 'center', borderRadius: 20 }}>
-        <Text style={{ fontSize: 25, fontWeight: 'bold' ,color:'black'}}>
+      <View style={{ height: 300, width: 300, backgroundColor: '#cfcfcf', justifyContent: 'center', alignItems: 'center', borderRadius: 20 , flexDirection:'row'}}>
+        <Text style={{ fontSize: 25, fontWeight: 'bold', color: 'black' }}>
           Select Gender :
         </Text>
         <Text>  {' '}  </Text>
-        <Button title='Male' onPress={() => navigation.navigate('Male Screen')} />
+        <Button title='Male' onPress={() => setSelectedGender('Male Screen')} />
         <Text> {' '} </Text>
-        <Button title='Female' onPress={() => navigation.navigate('Female Screen')} />
+        <Button title='Female' onPress={() => setSelectedGender('Female Screen')} />
       </View>
+        <Button  title="Next" onPress={handleNext} />
     </View>
   )
 }
