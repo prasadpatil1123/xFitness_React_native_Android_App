8284f3299b981a83a0cd5a44b21e2a45fbc817ab
prasadpatil1123 <122286888+prasadpatil1123@users.noreply.github.com>
2023-10-03 12:29:05 +0530

    Gojo2

--- src/StartScreen/Login.js
+++ src/StartScreen/Login.js
@@ -1,27 +1,122 @@
-import { StyleSheet, Text, View,Button,TextInput } from 'react-native'
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image } from 'react-native'
 import React, { useState, useEffect } from 'react'
-import Home from '../DashBoard/Home';
 
-const Login = ({navigation}) => {
 
-    const [email, setEmail] = useState('');
-    const [password, setpassword] = useState('');
-  
-    const handleLogin = () => {
-      if (email && password) {
-        navigation.navigate('Home Screen')
-      } else {
-        alert('Please fill in both email and password fields.');
-      }
+const Login = ({ navigation }) => {
+
+  const [email, setEmail] = useState('');
+  const [password, setpassword] = useState('');
+
+  const handleLogin = () => {
+    if (email && password) {
+      navigation.navigate('Home Screen')
+    } else {
+      alert('Please fill in both email and password fields.');
     }
+  }
 
   return (
-    <View>
-      
+    <View style={{ backgroundColor: 'black', flex: 1 }}>
+      <View>
+        <Image source={require("../assets/image/dumb1.jpeg")}
+          style={styles.ImgBox}
+        />
+      </View>
+      <View style={styles.LogBox}>
+        <View>
+
+          <Text style={styles.TxtQ} > Email Address</Text>
+          <TextInput
+            placeholder='Enter Email'
+            style={styles.TxtA}
+            onChangeText={text => setEmail(text)}
+            value={email}
+          />
+
+          <Text style={styles.TxtQ} > Password</Text>
+          <TextInput
+            placeholder='Enter Password'
+            onChangeText={text => setpassword(text)}
+            value={password}
+            secureTextEntry={true}
+            style={styles.TxtA}
+          />
+
+          <TouchableOpacity
+            onPress={() => navigation.navigate('SignUp Screen')}
+          >
+            <Text style={styles.forgotText}>Forgot Password</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={handleLogin}
+            style={styles.LoginButton}
+          >
+            <Text style={styles.LoginButtonTxt}>Login</Text>
+          </TouchableOpacity>
+        </View>
+        <Text style={styles.ORtxt}>Or</Text>
+        <View style={styles.SocialBox}>
+          <TouchableOpacity style={styles.Tiles}>
+            <Image source={require('../assets/icons/apple.png')} />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.Tiles}>
+            <Image source={require('../assets/icons/facebook.png')} />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.Tiles}>
+            <Image source={require('../assets/icons/google.png')} />
+          </TouchableOpacity>
+        </View>
+        <View>
+          <Text style={styles.RemTxt}>
+            Don't have an account?
+          </Text>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('SignUp Screen')}
+          >
+            <Text style={styles.RemButton}>Sign Up</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   )
 }
 
 export default Login
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  ImgBox: { width: 350, height: 250, justifyContent: 'center', marginTop: 0, marginLeft: 25 },
+  LogBox: {
+    flex: 1, backgroundColor: 'white', borderTopLeftRadius: 50, borderTopRightRadius: 50
+  },
+  TxtQ: {
+    marginTop: 30, marginLeft: 40, fontSize: 18, color: 'black'
+  },
+  TxtA: {
+    marginLeft: 35, fontSize: 17, color: 'black', backgroundColor: '#e4e3e3', marginRight: 35, borderRadius: 35,
+  },
+  forgotText: {
+    color: 'black', marginTop: 10, textAlign: 'right', marginRight: 37,
+  },
+  LoginButton: {
+    backgroundColor: 'gold', borderRadius: 15, marginTop: 15, marginLeft: 35, marginRight: 35, padding: 15, color: 'black'
+  },
+  LoginButtonTxt: {
+    textAlign: 'center', fontSize: 17, fontWeight: 'bold',
+  },
+  ORtxt: {
+    textAlign: 'center', paddingTop: 19, paddingBottom: 19, color: 'black', fontSize: 18, fontWeight: 'bold'
+  },
+  SocialBox: {
+    flexDirection: 'row', justifyContent: 'space-evenly'
+  },
+  Tiles: {
+    padding: 5, backgroundColor: '#e4e3e3', borderRadius: 15
+  },
+  RemTxt: {
+    color: 'black', marginTop: 20, textAlign: 'center'
+  },
+  RemButton: {
+    textAlign: 'center', color: 'blue'
+  },
+})
--- src/StartScreen/SignUp.js
+++ src/StartScreen/SignUp.js
@@ -1,106 +1,85 @@
-import { StyleSheet, Text, View,Button,TextInput } from 'react-native'
-import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native'
+import React, { useState, } from 'react'
 import Login from './Login';
 
-const SignUp = ({navigation}) => {
+const SignUp = ({ navigation }) => {
 
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setpassword] = useState('');
-  
-    const handleSignUp = () => {
-      if (name && email && password) {
-        navigation.navigate('Login Screen')
-      } else {
-        alert('Please fill in all fields.');
-      }
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setpassword] = useState('');
+
+  const handleSignUp = () => {
+    if (name && email && password) {
+      navigation.navigate('Login Screen')
+    } else {
+      alert('Please fill in all fields.');
     }
+  }
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 20,
-        backgroundColor: '#7cb9eB',
-      }}>
-      <View
-        style={{
-          height: 400,
-          width: 400,
-          backgroundColor: 'yellow',
-          justifyContent: 'center',
-          alignItems: 'center',
-          borderRadius: 20
-        }}>
-        <Text
-          style={{
-            fontSize: 25,
-            fontWeight: 'bold'
-          }}>
-          SignUp Screen
-        </Text>
-        <Text> {' '} </Text>
-        <TextInput
-          style={{
-            height: 40,
-            width: 250,
-            borderColor: 'gray',
-            borderWidth: 1,
-            marginTop: 20,
-            paddingHorizontal: 10,
-          }}
-          placeholder='Name'
-          onChangeText={text => setName(text)}
-          value={name}
+    <View style={{ backgroundColor: 'black', flex: 1 }}>
+      <View>
+        <Image source={require("../assets/image/dumb1.jpeg")}
+          style={styles.ImgBox}
         />
-        <TextInput
-          style={{
-            height: 40,
-            width: 250,
-            borderColor: 'gray',
-            borderWidth: 1,
-            marginTop: 20,
-            paddingHorizontal: 10,
-          }}
-          placeholder='Email'
-          onChangeText={text => setEmail(text)}
-          value={email}
-        />
-        <TextInput
-          style={{
-            height: 40,
-            width: 250,
-            borderColor: 'gray',
-            borderWidth: 1,
-            marginTop: 20,
-            paddingHorizontal: 10,
-          }}
-          placeholder='Password'
-          onChangeText={text => setpassword(text)}
-          value={password}
-          secureTextEntry={true}
-        />
-        <TextInput
-          style={{
-            height: 40,
-            width: 250,
-            borderColor: 'gray',
-            borderWidth: 1,
-            marginTop: 20,
-            paddingHorizontal: 10,
-          }}
-          placeholder='Confirm Password'
-          onChangeText={text => setpassword(text)}
-          value={password}
-          secureTextEntry={true}
-        />
-        <Text> {' '} </Text>
-        <Button
-          title='Sign Up'
-          onPress={handleSignUp}
-        />
+      </View>
+      <View style={styles.SignBox}>
+        <View>
+
+          <Text style={styles.TxtQ} >Full Name</Text>
+          <TextInput
+            placeholder='Enter Full Name'
+            style={styles.TxtA}
+            onChangeText={text => setName(text)}
+            value={name}
+          />
+
+          <Text style={styles.TxtQ} >Email Address</Text>
+          <TextInput
+            placeholder='Enter Email Address'
+            style={styles.TxtA}
+            onChangeText={text => setEmail(text)}
+            value={email}
+          />
+
+          <Text style={styles.TxtQ} > Password</Text>
+          <TextInput
+            placeholder='Enter Password'
+            onChangeText={text => setpassword(text)}
+            value={password}
+            secureTextEntry={true}
+            style={styles.TxtA}
+          />
+
+          <TouchableOpacity
+            onPress={handleSignUp}
+            style={styles.SignUpButton}
+          >
+            <Text style={styles.SignUpButtonTxt}>Sign Up</Text>
+          </TouchableOpacity>
+        </View>
+        <Text style={styles.ORtxt}>Or</Text>
+        <View style={styles.SocialBox}>
+          <TouchableOpacity style={styles.Tiles}>
+            <Image source={require('../assets/icons/apple.png')} />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.Tiles}>
+            <Image source={require('../assets/icons/facebook.png')} />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.Tiles}>
+            <Image source={require('../assets/icons/google.png')} />
+          </TouchableOpacity>
+        </View>
+        <View>
+          <Text style={styles.RemTxt}>
+          Already have an account?
+          </Text>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('Login Screen')}
+          >
+            <Text style={styles.RemButton}>Login</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   )
@@ -108,4 +87,24 @@ const SignUp = ({navigation}) => {
 
 export default SignUp
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  ImgBox: { width: 230, height: 150, justifyContent: 'center', marginLeft: 90 },
+  SignBox: { marginTop: 50, flex: 1, backgroundColor: 'white', borderTopLeftRadius: 50, borderTopRightRadius: 50 },
+  TxtQ: { marginTop: 30, marginLeft: 40, fontSize: 18, color: 'black' },
+  TxtA: { marginLeft: 35, fontSize: 17, color: 'black', backgroundColor: '#e4e3e3', borderRadius: 15, marginRight: 35 },
+  SignUpButton:{backgroundColor: 'gold', borderRadius: 15, marginTop: 15, marginLeft: 35, marginRight: 35, padding: 15, color: 'black'},
+  SignUpButtonTxt:{ textAlign: 'center', fontSize: 17, fontWeight: 'bold',},
+  ORtxt:{textAlign: 'center', paddingTop: 19, paddingBottom: 19, color: 'black', fontSize: 18, fontWeight: 'bold'},
+  SocialBox: {
+    flexDirection: 'row', justifyContent: 'space-evenly'
+  },
+  Tiles: {
+    padding: 5, backgroundColor: '#e4e3e3', borderRadius: 15
+  },
+  RemTxt: {
+    color: 'black', marginTop: 20, textAlign: 'center'
+  },
+  RemButton: {
+    textAlign: 'center', color: 'blue'
+  },
+})
